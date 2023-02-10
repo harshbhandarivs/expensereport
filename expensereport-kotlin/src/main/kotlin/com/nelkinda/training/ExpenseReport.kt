bcdcfe5841a4145dd6f3884e92c966949fb6aae8
Harsh Bhandari <harshb@sahaj.ai>
2023-02-10 10:30:59 +0530

    Lifted expenseName from when block

--- expensereport-kotlin/src/main/kotlin/com/nelkinda/training/ExpenseReport.kt
+++ expensereport-kotlin/src/main/kotlin/com/nelkinda/training/ExpenseReport.kt
@@ -23,11 +23,10 @@ class ExpenseReport(
                 mealExpenses += expense.amount
             }
 
-            var expenseName = ""
-            when (expense.type) {
-                ExpenseType.DINNER -> expenseName = "Dinner"
-                ExpenseType.BREAKFAST -> expenseName = "Breakfast"
-                ExpenseType.CAR_RENTAL -> expenseName = "Car Rental"
+            val expenseName = when (expense.type) {
+                ExpenseType.DINNER -> "Dinner"
+                ExpenseType.BREAKFAST -> "Breakfast"
+                ExpenseType.CAR_RENTAL -> "Car Rental"
             }
 
             val mealOverExpensesMarker =
